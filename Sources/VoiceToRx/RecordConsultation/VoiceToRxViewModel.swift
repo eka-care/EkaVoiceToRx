7596b6832156127b50556049e9542c55ca502f06
aryaIOS <arya.vashisht@eka.care>
2025-06-11 15:20:05 +0530

    Fixed syntax

--- Sources/VoiceToRx/RecordConsultation/VoiceToRxViewModel.swift
+++ Sources/VoiceToRx/RecordConsultation/VoiceToRxViewModel.swift
@@ -304,7 +304,7 @@ public final class VoiceToRxViewModel: ObservableObject {
   
   func deleteRecording(id: UUID) {
     voiceToRxRepo.deleteVoiceConversation(fetchRequest: QueryHelper.fetchRequest(for: id))
-    screenState == .deletedRecording
+    screenState = .deletedRecording
   }
 }
 
