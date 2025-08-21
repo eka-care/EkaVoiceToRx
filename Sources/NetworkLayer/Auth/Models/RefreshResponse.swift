96a469de1ed0de6270a2d724218342c1b7aad437
BRUNDAB19B <brunda.b@eka.care>
2025-08-21 22:21:05 +0530

    RefreshResponse model fixed

--- Sources/NetworkLayer/Auth/Models/RefreshResponse.swift
+++ Sources/NetworkLayer/Auth/Models/RefreshResponse.swift
@@ -10,37 +10,12 @@ import Foundation
 
 // MARK: - RefreshResponse
 struct RefreshResponse: Codable {
-  let success: Bool?
-  let data: RefreshDataClass?
-}
-
-// MARK: - DataClass
-struct RefreshDataClass: Codable {
-  let resJwt: String?
-  let profile: RefreshProfile?
-  let sess, refresh: String?
-  let new: Bool?
-  let firebaseToken: String?
+  let refresh: String?
+  let sess: String?
   
   enum CodingKeys: String, CodingKey {
-    case firebaseToken = "firebase_token"
-    case resJwt, profile, sess, refresh, new
+    case refresh = "refresh_token"
+    case sess = "access_token"
   }
 }
-
-// MARK: - Profile
-struct RefreshProfile: Codable {
-  let uuid, mobile: String?
-  let isP: Bool?
-  let mn, ln, at: String?
-  let dobValid: Bool?
-  let dob, fn, oid, gen: String?
-  
-  enum CodingKeys: String, CodingKey {
-    case uuid, mobile
-    case isP = "is-p"
-    case mn, ln, at
-    case dobValid = "dob-valid"
-    case dob, fn, oid, gen
-  }
-}
+ 
