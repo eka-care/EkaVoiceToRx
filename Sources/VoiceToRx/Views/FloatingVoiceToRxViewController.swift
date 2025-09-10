6f7f987ee93cb5053a563cf6722ec3359fabae12
BRUNDAB19B <brunda.b@eka.care>
2025-09-10 14:54:44 +0530

    fix top view controller floating button

--- Sources/VoiceToRx/Views/FloatingVoiceToRxViewController.swift
+++ Sources/VoiceToRx/Views/FloatingVoiceToRxViewController.swift
@@ -157,8 +157,9 @@ public class FloatingVoiceToRxViewController: UIViewController {
       }
     ))
     
-    let controller = keyWindow?.rootViewController
-    controller?.present(alertController, animated: true)
+    if let topVC = UIApplication.shared.topMostViewController() {
+      topVC.present(alertController, animated: true)
+    }
   }
   
   @objc private func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
@@ -299,4 +300,23 @@ extension FloatingVoiceToRxViewController {
   private func getAmazonCredentials() {
     viewModel?.getAmazonCredentials()
   }
+}
+
+extension UIApplication {
+  func topMostViewController(base: UIViewController? = UIApplication.shared.connectedScenes
+    .compactMap { $0 as? UIWindowScene }
+    .flatMap { $0.windows }
+    .first { $0.isKeyWindow }?.rootViewController) -> UIViewController? {
+      
+      if let nav = base as? UINavigationController {
+        return topMostViewController(base: nav.visibleViewController)
+      }
+      if let tab = base as? UITabBarController {
+        return topMostViewController(base: tab.selectedViewController)
+      }
+      if let presented = base?.presentedViewController {
+        return topMostViewController(base: presented)
+      }
+      return base
+    }
 }
