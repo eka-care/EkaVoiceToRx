4626e9b8e703345b3696f2baccbb46ddc76af83a
sahithiim <sahithi.mucchala@eka.care>
2026-01-08 13:02:51 +0530

    Past sessions api failing issue fixed (#44)

--- Sources/NetworkLayer/VoiceToRx/Models/EkaScribeHistoryResponse.swift
+++ Sources/NetworkLayer/VoiceToRx/Models/EkaScribeHistoryResponse.swift
@@ -19,42 +19,64 @@ public struct EkaScribeHistoryResponse: Codable {
 }
 
 public struct ScribeData: Codable {
-    public let bID: String
-    public let createdAt: String
-    public let flavour: Flavour?
-    public let mode: Mode
-    public let oid: String
-    public let processingStatus: ProcessingStatus
-    public let txnID: String
-    public let userStatus: UserStatus
-    public let uuid, version: String?
-    public let patientDetails: PatientDetails?
+  public let bID: String?
+  public let createdAt: String?
+  private let flavourString: String?
+  private let modeString: String?
+  public let oid: String?
+  private let processingStatusString: String?
+  public let txnID: String?
+  private let userStatusString: String?
+  public let uuid, version: String?
+  public let patientDetails: PatientDetails?
   
-    public enum CodingKeys: String, CodingKey {
-        case bID = "b_id"
-        case createdAt = "created_at"
-        case flavour, mode, oid
-        case processingStatus = "processing_status"
-        case txnID = "txn_id"
-        case userStatus = "user_status"
-        case uuid, version
-        case patientDetails = "patient_details"
-    }
+  public var flavour: Flavour? {
+    guard let flavourString else { return nil }
+    return Flavour(rawValue: flavourString)
+  }
+  
+  public var mode: Mode? {
+    .init(rawValue: modeString ?? "")
+  }
+  
+  public var processingStatus: ProcessingStatus? {
+    .init(rawValue: processingStatusString ?? "")
+  }
+  
+  public var userStatus: UserStatus? {
+    .init(rawValue: userStatusString ?? "")
+  }
+  
+  public enum CodingKeys: String, CodingKey {
+    case bID = "b_id"
+    case createdAt = "created_at"
+    case flavourString = "flavour"
+    case modeString = "mode"
+    case oid
+    case processingStatusString = "processing_status"
+    case txnID = "txn_id"
+    case userStatusString = "user_status"
+    case uuid, version
+    case patientDetails = "patient_details"
+  }
 }
 
-public enum Flavour: String, Codable {
-    case android = "android"
-    case empty = ""
-    case flavourExtension = "extension"
-    case io = "io"
-    case ip = "ip"
-    case web = "web"
+public enum Flavour: String {
+  case android
+  case empty = ""
+  case flavourExtension = "extension"
+  case io
+  case ip
+  case web
+  case scribeAndroid = "scribe-android"
+  case scribeIOS = "scribe-ios"
 }
 
-public enum Mode: String, Codable {
-    case consultation = "consultation"
-    case dictation = "dictation"
+public enum Mode: String {
+  case consultation
+  case dictation
 }
+
 
 public struct PatientDetails: Codable {
     public let oid: String?
@@ -69,18 +91,18 @@ public enum Status: String, Codable {
     case systemFailure = "system_failure"
 }
 
-public enum UserStatus: String, Codable {
-    case commit = "commit"
+public enum UserStatus: String {
+    case commit
     case userInit = "init"
-    case stopped = "stopped"
-    case cancelled = "cancelled"
+    case stopped
+    case cancelled
 }
 
-public enum ProcessingStatus: String, Codable {
-  case success = "success"
+public enum ProcessingStatus: String {
+  case success
   case inProgress = "in-progress"
   case systemFailure = "system_failure"
   case requestFailure = "request_failure"
-  case cancelled = "cancelled"
+  case cancelled
 }
 
