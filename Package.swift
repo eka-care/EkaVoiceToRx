b7298198ae1d45f06df80a83c2bc8d7d1d642d51
BRUNDAB19B <brunda.b@eka.care>
2026-01-14 15:01:35 +0530

    Remove ekaui package (#53)

--- Package.swift
+++ Package.swift
@@ -20,8 +20,7 @@ let package = Package(
       .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .upToNextMajor(from: "5.0.0")),
       .package(url: "https://github.com/gfreezy/libfvad.git", .upToNextMajor(from: "1.0.0")),
       .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.2")),
-      .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-      .package(url: "git@github.com:eka-care/EkaUI.git", branch: "main")
+      .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0")
     ],
     targets: [
         .target(
@@ -32,8 +31,7 @@ let package = Package(
               .product(name: "SwiftyJSON", package: "SwiftyJSON"),
               .product(name: "libfvad", package: "libfvad"),
               .product(name: "Alamofire", package: "Alamofire"),
-              .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-              .product(name: "EkaUI", package: "EkaUI")
+              .product(name: "SwiftProtobuf", package: "swift-protobuf"), 
             ],
             resources: [
               .process("Resources")
