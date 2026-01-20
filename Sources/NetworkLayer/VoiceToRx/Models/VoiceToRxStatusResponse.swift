9e84f39f7ba36819ed3b950bc698e0c0063ff16d
BRUNDAB19B <brunda.b@eka.care>
2026-01-20 20:13:16 +0530

    fix the templates fetch  (#55)

--- Sources/NetworkLayer/VoiceToRx/Models/VoiceToRxStatusResponse.swift
+++ Sources/NetworkLayer/VoiceToRx/Models/VoiceToRxStatusResponse.swift
@@ -25,10 +25,12 @@ public struct VoiceToRxStatusData: Codable {
 
 public struct TemplateResults: Codable {
 
+  public var integration: [VoiceToRxOutput]?
   public var custom: [VoiceToRxOutput]?
   public var transcript: [VoiceToRxOutput]?
   
   enum CodingKeys: String, CodingKey {
+    case integration
     case custom
     case transcript
   }
